c200373ca268a74bc5246e98ecd1ba0392d01d19
DeeJay03 <djantoinette20@gmail.com>
2015-08-16 22:28:11 +1000

    Web page split into separate files

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-  include('html/header.html');
+   include('html/header.html');
   
-  if(isset($_GET['search'])) {
-    include('php/MovieList.php');
-  }
-  else {
-    echo "<br /><div class = 'title' align = 'center'>Please select above</div>";
-  }
+   if(isset($_GET['search'])) {
+     include('php/MovieList.php');
+   }
+   else {
+     echo "<br /><div class = 'title' align = 'center'>Please select above</div>";
+   }
   
-  include('html/footer.html');
+   include('html/footer.html');
 	
 ?>
