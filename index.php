6d87655ff1d860afa16403f74aa7372a2c536dd2
DeeJay03 <djantoinette20@gmail.com>
2015-08-16 22:10:15 +1000

    Main page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+  include('html/header.html');
+  
+  if(isset($_GET['search'])) {
+    include('php/MovieList.php');
+  }
+  else {
+    echo "<br /><div class = 'title' align = 'center'>Please select above</div>";
+  }
+  
+  include('html/footer.html');
+	
+?>
